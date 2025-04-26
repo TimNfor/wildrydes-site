6c45aea7af6660d45295087e7f03903e0575723e
TimNfor <159589312+TimNfor@users.noreply.github.com>
2025-04-26 17:57:24 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,6 +3,7 @@ window._config = {
         userPoolId: 'us-east-1_YO0QbyUQb', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '2hfe8jtttiua3vn2e8gjk70ub1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
+        userPoolClientSecret: '1nk3fnqvngsph80sjvdk4vdpfepuncvmgnrh1orci9jb5t0tl8uo'
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
